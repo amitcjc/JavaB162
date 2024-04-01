d5838c43082883b0715747d1669c79f7d516d92e
amitcjc <156275076+amitcjc@users.noreply.github.com>
2024-04-01 13:35:14 +0530

    by Sr

--- GitDemob162/src/com/Demo.java
+++ GitDemob162/src/com/Demo.java
@@ -7,7 +7,10 @@ public class Demo
 		System.out.println("This is M1");
 	}
 	
-	
+	public void m2()
+	{
+		System.out.println("This is M2");
+	}
 	
 public static void main(String[] args) {
 	System.out.println("This is Git Demo");
